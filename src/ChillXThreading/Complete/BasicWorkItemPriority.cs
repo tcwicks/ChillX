3aac1193c00cb8edad875264165f33a7496e894e
Wickramasingha, Tikiri <tcwicks@gmail.com>
2022-04-07 19:47:09 +1200

    Added default BasicWorkItemPriority with predefined basic processing priorities

---
+++ src/ChillXThreading/Complete/BasicWorkItemPriority.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChillXThreading.Complete
+{
+    public enum BasicWorkItemPriority
+    {
+        Low = 0,
+        Medium = 1,
+        High = 2,
+    }
+}
